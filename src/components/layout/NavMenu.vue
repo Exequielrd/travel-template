4bff13d855a6e7fdae102b3c4be07d8e3be81ef0
Exequiel <exequielrd10@gmail.com>
2023-04-28 22:14:44 -0300

    nav style

--- src/components/layout/NavMenu.vue
+++ src/components/layout/NavMenu.vue
@@ -1,3 +1,7 @@
+<script setup>
+const navItems = ['Home', 'About Us', 'Contact']
+</script>
+
 <template>
   <header
     class="w-full h-[75px] fixed top-0 bg-purple/70 z-50 flex items-center backdrop-blur-md text-white px-2"
@@ -6,10 +10,13 @@
       <div class="aspect-square w-[50px] bg-orange rounded-full"></div>
       <nav class="hidden md:flex items-center gap-6">
         <ul class="flex gap-6">
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact</li>
-          <li>Newsletter</li>
+          <li
+            v-for="item of navItems"
+            :key="item"
+            class="border-b hover:border-white cursor-pointer border-transparent"
+          >
+            {{ item }}
+          </li>
         </ul>
       </nav>
     </div>
