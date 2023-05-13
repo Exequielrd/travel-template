994da4583f70e2a0a0572f69677bff9ebf12a47d
Exequiel <exequielrd10@gmail.com>
2023-05-13 17:11:26 -0300

    cards section

--- src/components/sections/CardsSection.vue
+++ src/components/sections/CardsSection.vue
@@ -1,7 +1,16 @@
-<script setup></script>
+<script setup>
+import SimpleCard from '../shared/SimpleCard.vue'
+</script>
 
 <template>
-  <div class="w-full min-h-[70vh]">
-    <h2>Proximamente</h2>
+  <div class="w-full min-h-[70vh] flex flex-col justify-center items-center">
+    <h2 class="text-4xl font-bold text-center">Lorem ipsum dolor.</h2>
+    <p class="text-center text-lg mt-5 mb-10 max-w-[65ch]">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, ratione culpa. Magnam
+      veritatis aperiam
+    </p>
+    <div class="flex gap-5">
+      <SimpleCard v-for="i of 3" :key="i" />
+    </div>
   </div>
 </template>
